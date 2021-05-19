08db870ed1e7222e7e091e54b59dfbbdc4df51aa
Mimi-codes <65224611+Mimi-codes@users.noreply.github.com>
2021-05-19 22:04:43 +0100

    Add files via upload
    First commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const content_1 = document.getElementById('luke');
+content_1.addEventListener('click', function () {
+    content_1.innerHTML = '<ul>Height: 172</ul> <ul>Gender: Male</ul> <ul>Name: Luke Skywalker</ul>';
+});
+
+const content_2 = document.getElementById('po'); 
+    content_2.addEventListener('click', function () {
+        content_2.innerHTML = '<ul>Height: 167</ul> <ul>Gender: N/A</ul> <ul>Name: C-3PO</ul>';
+    });  
+
+    const content_3 = document.getElementById('d2'); 
+    content_3.addEventListener('click', function () {
+        content_3.innerHTML = '<ul>Height: 96</ul> <ul>Gender: N/A</ul> <ul>Name: R2-D2</ul>';
+    });  
+
+    const content_4 = document.getElementById('darth'); 
+    content_4.addEventListener('click', function () {
+        content_4.innerHTML = '<ul>Height: 202</ul> <ul>Gender: Male</ul> <ul>Name: Darth Vader</ul>';
+    });  
+
+    const content_5 = document.getElementById('leia'); 
+    content_5.addEventListener('click', function () {
+        content_5.innerHTML = '<ul>Height: 150</ul> <ul>Gender: Female</ul> <ul>Name: Leia Organa</ul>';
+    });  
+
+    const content_6 = document.getElementById('owen'); 
+    content_6.addEventListener('click', function () {
+        content_6.innerHTML = '<ul>Height: 178</ul> <ul>Gender: Male</ul> <ul>Name: Owen Lars</ul>';
+    });  
+
+    const content_7 = document.getElementById('beru'); 
+    content_7.addEventListener('click', function () {
+        content_7.innerHTML = '<ul>Height: 165</ul> <ul>Gender: Female</ul> <ul>Name: Beru Whitesunlars</ul>';
+    });  
+
+    const content_8 = document.getElementById('d4'); 
+    content_8.addEventListener('click', function () {
+        content_8.innerHTML = '<ul>Height: 97</ul> <ul>Gender: N/A</ul> <ul>Name: R5-D4</ul>';
+    });  
+
+    const content_9 = document.getElementById('biggs'); 
+    content_9.addEventListener('click', function () {
+        content_9.innerHTML = '<ul>Height: 183</ul> <ul>Gender: Male</ul> <ul>Name: Biggs Darklighter</ul>';
+    });  
+
+    const content_10 = document.getElementById('obi'); 
+    content_10.addEventListener('click', function () {
+        content_10.innerHTML = '<ul>Height: 182</ul> <ul>Gender: Male</ul> <ul>Name: Obi-Wan Kenobi</ul>';
+    });  
